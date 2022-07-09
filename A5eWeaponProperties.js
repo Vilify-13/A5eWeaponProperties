e2090ca0f4fbb75368e6f662102850ece091181b
Vilify-13 <109003055+Vilify-13@users.noreply.github.com>
2022-07-09 17:11:37 -0500

    Add files via upload

---
+++ A5eWeaponProperties.js
@@ -0,0 +1,12 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['breaker'] = 'Breaker';
+  	CONFIG.DND5E.weaponProperties['compounding'] = 'Compounding';
+	CONFIG.DND5E.weaponProperties['defensive'] = 'Defensive';
+	CONFIG.DND5E.weaponProperties['dualwielding'] = 'Dual-Wielding';
+	CONFIG.DND5E.weaponProperties['handmounted'] = 'Hand-Mounted';
+	CONFIG.DND5E.weaponProperties['parrying'] = 'Parrying';
+	CONFIG.DND5E.weaponProperties['parryingimmunity'] = 'Parrying Immunity';
+	CONFIG.DND5E.weaponProperties['trip'] = 'Trip';
+	CONFIG.DND5E.weaponProperties['vicious'] = 'Vicious';
+	
+});
