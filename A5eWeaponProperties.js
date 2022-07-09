7884ab43d77945e010cfef1579dc44939c166d3a
Vilify-13 <109003055+Vilify-13@users.noreply.github.com>
2022-07-09 17:18:45 -0500

    Update A5eWeaponProperties.js

--- A5eWeaponProperties.js
+++ A5eWeaponProperties.js
@@ -5,7 +5,7 @@ Hooks.on('ready', () => {
 	CONFIG.DND5E.weaponProperties['dualwielding'] = 'Dual-Wielding';
 	CONFIG.DND5E.weaponProperties['handmounted'] = 'Hand-Mounted';
 	CONFIG.DND5E.weaponProperties['parrying'] = 'Parrying';
-	CONFIG.DND5E.weaponProperties['parryingimmunity'] = 'Parrying Immunity';
+	CONFIG.DND5E.weaponProperties['parryingimmune'] = 'Parry-Immune';
 	CONFIG.DND5E.weaponProperties['trip'] = 'Trip';
 	CONFIG.DND5E.weaponProperties['vicious'] = 'Vicious';
 	
